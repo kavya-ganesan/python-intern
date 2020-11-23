894d82d0ef190ad4b2e81e00c537ff7b9d1d0fab
kavya.v.g <74829156+kavya-ganesan@users.noreply.github.com>
2020-11-23 19:10:08 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,6 +1,7 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> string ="good morning all"
->>> print(string[1:])
+#Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+#Type "help", "copyright", "credits" or "license()" for more information.
+string ="good morning all"
+print(string[1:])
+#output
 ood morning all
->>> 
+ 
