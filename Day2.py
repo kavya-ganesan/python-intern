3904f1ca3be2dd0aea01de10de8ed92894534f24
kavya.v.g <74829156+kavya-ganesan@users.noreply.github.com>
2020-11-24 12:25:50 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,14 @@
+a=b=c=100
+res1=a/10
+res2=b*50
+res3=c+60
+print("res1=",res1)
+print("res2=",res2)
+print("res3=",res3)
+s="apples"
+a=s.replace('l','G')
+print(a)
+a1=10
+b1=0.50
+print(float(a))
+print(int(b))
