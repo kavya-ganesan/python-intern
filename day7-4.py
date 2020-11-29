ce3ef0ca8fd0892a7ff1105609a9e812aa615f18
kavya.v.g <74829156+kavya-ganesan@users.noreply.github.com>
2020-11-29 14:46:59 +0530

    Update day7-4.py

--- day7-4.py
+++ day7-4.py
@@ -2,3 +2,13 @@ import sys
 for p in sys.path:
     print("the path is",p)
 
+"""
+output
+the path is K:/python inter
+the path is C:\Users\HP\AppData\Local\Programs\Python\Python38-32\Lib\idlelib
+the path is C:\Users\HP\AppData\Local\Programs\Python\Python38-32\python38.zip
+the path is C:\Users\HP\AppData\Local\Programs\Python\Python38-32\DLLs
+the path is C:\Users\HP\AppData\Local\Programs\Python\Python38-32\lib
+the path is C:\Users\HP\AppData\Local\Programs\Python\Python38-32
+the path is C:\Users\HP\AppData\Local\Programs\Python\Python38-32\lib\site-packages
+"""
