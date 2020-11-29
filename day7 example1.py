6677aad754ba3659fe608e66c1500d44110419c7
kavya.v.g <74829156+kavya-ganesan@users.noreply.github.com>
2020-11-29 13:58:29 +0530

    Update day7 example1.py

--- day7 example1.py
+++ day7 example1.py
@@ -2,3 +2,13 @@ import day7example
 print("the list is",day7example.l)
 day7example.l[3]=12
 print("the new list is",day7example.l)
+
+"""
+output
+the list is [1, 2, 3, 4, 5]
+the new list is [1, 2, 3, 12, 5]
+>>> """
+"""
+day7example.py
+l=[1, 2, 3, 4, 5]
+"""
